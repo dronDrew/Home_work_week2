33fae7934e9052d8068503b3c23e53daf76f4712
Андрій Дудчак <andrew.melauda@gmail.com>
2020-03-12 20:42:03 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using myClass_lib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Soviet_tank = "T34";
+                string German_tank = "Pantera";
+                int tank_number = 5;
+                Tank[] crew_sov = new Tank[tank_number];
+                Tank[] crew_ger = new Tank[tank_number];
+                for (int i = 0; i < tank_number; i++)
+                {
+                    crew_sov[i] = new Tank(Soviet_tank);
+                    Thread.Sleep(100);
+                    crew_ger[i] = new Tank(German_tank);
+                }
+                for (int i = 0; i < tank_number; i++)
+                {
+                    Console.WriteLine(crew_sov[i] ^ crew_ger[i]);
+                }
+            }
+            catch (ArgumentException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ myClass_lib/WordofTanks/Tank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace myClass_lib
+{
+    public class Tank
+    {
+        private string Name;
+        private int Level_amunition;
+        private int Level_armor;
+        private int Level_maneuverability;
+        public Tank(string name) {
+            try
+            {
+                Random rand = new Random();
+                this.Name = name;
+                this.Level_amunition = rand.Next(0, 100);
+                this.Level_armor = rand.Next(0, 100);
+                this.Level_maneuverability = rand.Next(0, 100);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public override string ToString() {
+            return "Name "+this.Name +" Level amunition "+ this.Level_amunition +" Level armor "+ Level_armor + " Level maneuverability "+this.Level_maneuverability;
+        }
+        static public string operator ^(Tank a, Tank b) {
+            try
+            {
+                if ((a.Level_amunition > b.Level_amunition && a.Level_armor > b.Level_armor) || (a.Level_armor > b.Level_armor && a.Level_maneuverability > b.Level_maneuverability) || (a.Level_amunition > b.Level_amunition && a.Level_maneuverability > b.Level_maneuverability))
+            {
+                return a.ToString();
+            }
+            else if ((a.Level_amunition < b.Level_amunition && a.Level_armor < b.Level_armor) || (a.Level_armor < b.Level_armor && a.Level_maneuverability < b.Level_maneuverability) || (a.Level_amunition < b.Level_amunition && a.Level_maneuverability < b.Level_maneuverability)) {
+                return b.ToString();
+            }
+            else {
+                return "drow";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
